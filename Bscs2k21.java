9279af5ea972f27923f9d4dbc49a370da51a3ed2
Saad Bin Zahid <105521333+SaadBinZahid15@users.noreply.github.com>
2022-05-17 19:15:30 +0500

    Practice 2

---
+++ Bscs2k21.java
@@ -0,0 +1,20 @@
+public class Bscs2k21
+{
+	public static void main(String args[])
+	{
+		int Ni=20;
+		System.out.println(Ni);
+		float Nf=6.09f;
+		System.out.println(Nf);
+		String Ns="diluc";
+		System.out.println(Ns);
+		char Nc='l';
+		System.out.println(Nc);
+		long Nl=297l;
+		System.out.println(Nl);
+		double Nd=4.99d;
+		System.out.println(Nd);
+		boolean Nb=true;
+		System.out.println(Nb);
+	}
+}
