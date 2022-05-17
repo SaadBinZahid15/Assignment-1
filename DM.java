bf483de3fa989e7579dcb1d541fa2e3077cf8ac4
Saad Bin Zahid <105521333+SaadBinZahid15@users.noreply.github.com>
2022-05-17 19:16:30 +0500

    practice 3

---
+++ DM.java
@@ -0,0 +1,21 @@
+public class DM
+{
+	public static void main (String args[])
+	{
+		int array_month[]={31,28,30,31,30,31,30,31,30,31,30,31};
+		//array_month=new int[12];
+		//array_month[0]=31;
+		//array_month[2]=28;
+		//array_month[3]=31;
+		//array_month[4]=30;
+		//array_month[5]=31;
+		//array_month[6]=30;
+		//array_month[7]=31;
+		//array_month[8]=30;
+		//array_month[9]=31;
+		//array_month[10]=30;
+		//array_month[11]=31;
+
+		System.out.println(array_month[1]);
+	}
+}
