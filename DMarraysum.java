8b7cde1e0314ec67fbc27dd1dd4182a2e703b3b5
Saad Bin Zahid <105521333+SaadBinZahid15@users.noreply.github.com>
2022-07-26 09:56:10 -0700

    practice 9

---
+++ DMarraysum.java
@@ -0,0 +1,13 @@
+public class DMarraysum
+{
+	public static void main(String[] arg)
+	{
+		int[] s=new int[10];
+		int sum=0;
+		for(int i=0; i<s.length ; i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
